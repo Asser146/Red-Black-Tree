b631c00a22bd44759d19ebb124a8f15ebb714182
Iraqy <omariraqy16@gmail.com>
2023-04-05 06:46:30 +0200

    Added Search , treeSize Worked on insertion verification but still not finalized

--- RB_Tree.py
+++ RB_Tree.py
@@ -16,12 +16,16 @@ class Tree:
         self.root = self.nil
 
     def insert(self, data):
+        flag = self.search(self.root, data)
+        if flag is True:
+            print("ERROR: Word already in the dictionary!")
+            return
 
         newNode = TreeNode(data)
         newNode.parent = None
         newNode.left = self.nil
         newNode.right = self.nil
-        newNode.color ="red" 
+        newNode.color = "red"
         parent = None
         current = self.root
         while current != self.nil:
@@ -40,51 +44,59 @@ class Tree:
             parent.left = newNode
         else:
             parent.right = newNode
+
+        if newNode.parent == None:
+            newNode.color = "black"
+            return
+
+        if newNode.parent.parent == None:
+            return
+
         self.fixInsertion(newNode)
 
     def fixInsertion(self, newNode):
-        while newNode != self.root and newNode.parent.color=="red":
+        while newNode != self.root and newNode.parent.color == "red":
             if newNode.parent == newNode.parent.parent.right:
-                uncle= newNode.parent.parent.left  # uncle
-                if uncle.color=="red":
-                    uncle.color ="black"
-                    newNode.parent.color ="black"
-                    newNode.parent.parent.color ="red"
+                uncle = newNode.parent.parent.left  # uncle
+                if uncle.color == "red":
+                    uncle.color = "black"
+                    newNode.parent.color = "black"
+                    newNode.parent.parent.color = "red"
                     newNode = newNode.parent.parent
                 else:
                     if newNode == newNode.parent.left:
                         newNode = newNode.parent
                         self.rotateRight(newNode)
-                    newNode.parent.color ="black"
-                    newNode.parent.parent.color ="red"
+                    newNode.parent.color = "black"
+                    newNode.parent.parent.color = "red"
                     self.rotateLeft(newNode.parent.parent)
             else:
-                uncle= newNode.parent.parent.right  # uncle
+                uncle = newNode.parent.parent.right  # uncle
 
-                if uncle.color=="red":
-                    uncle.color ="black"
-                    newNode.parent.color ="black"
-                    newNode.parent.parent.color ="red"
+                if uncle.color == "red":
+                    uncle.color = "black"
+                    newNode.parent.color = "black"
+                    newNode.parent.parent.color = "red"
                     newNode = newNode.parent.parent
                 else:
                     if newNode == newNode.parent.right:
                         newNode = newNode.parent
                         self.rotateLeft(newNode)
-                    newNode.parent.color ="black"
-                    newNode.parent.parent.color ="red"
+                    newNode.parent.color = "black"
+                    newNode.parent.parent.color = "red"
                     self.rotateRight(newNode.parent.parent)
-        self.root.color ="black"
-        
-    def getHeight(self,node):
+        self.root.color = "black"
 
-        if node ==self.nil:       
-            return -1 
+    def getHeight(self, node):
+
+        if node == self.nil:
+            return -1
         leftHeight = self.getHeight(node.left)
         rightHeight = self.getHeight(node.right)
-    
-        return max(leftHeight, rightHeight)+1
 
-    def rotateLeft(self,x):
+        return max(leftHeight, rightHeight) + 1
+
+    def rotateLeft(self, x):
         y = x.right
         x.right = y.left
         if y.left != self.nil:
@@ -100,7 +112,7 @@ class Tree:
         y.left = x
         x.parent = y
 
-    def rotateRight(self,x):
+    def rotateRight(self, x):
         y = x.left
         x.left = y.right
         if y.right != self.nil:
@@ -115,10 +127,36 @@ class Tree:
             x.parent.left = y
         y.right = x
         x.parent = y
-        
+
     def printInorder(self, node):
         if node:
             self.printInorder(node.left)
-            if node!=self.nil:
+            if node != self.nil:
                 print(f"Data= {node.data} , color= {node.color}")
             self.printInorder(node.right)
+
+    def search(self, node, data):
+        if (node.data == None):
+            # print("Not Found")
+            return False
+        elif (node.data == data):
+            print("Found")
+            return True
+        elif data < node.data:
+            if node.left is None:
+                return False
+            else:
+                # print("Went left")
+                self.search(node.left, data)
+        else:
+            if node.right is None:
+                return False
+            else:
+                # print("Went right")
+                self.search(node.right, data)
+
+    def treeSize(self, node):
+        if node.data == None:
+            return 0
+        else:
+            return self.treeSize(node.left) + 1 + self.treeSize(node.right)
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from RB_Tree import Tree
 
-tree=Tree()
-for i in range (1,11):
+tree = Tree()
+for i in range(1, 11):
     tree.insert(i)
 
-tree.printInorder(tree.root)
 
-print(f"Height= {tree.getHeight(tree.root)}")
-
-
-
-
-        
-    
+if tree.search(tree.root,2):
+    print("Main Found")
 
 
 
+# tree.printInorder(tree.root)
+#
+# print(f"Height= {tree.getHeight(tree.root)}")
+#
+# tree.search(tree.root,9)
+#
+# print(tree.treeSize(tree.root))
